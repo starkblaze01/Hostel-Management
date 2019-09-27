0387a03209e3b3684df70e46e3a6d4bb976af2d6
Anshuman Verma <anshu.av97@gmail.com>
2019-09-27 10:45:04 +0530

    feat: student get, post

---
+++ models/Student.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const StudentSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  batch: {
+    type: Number,
+    required: true,
+    min: 2016,
+    max: 2020
+
+  },
+  id: {
+    type: String,
+    unique: true,
+  },
+  room: {
+    type: String,
+  }
+  ,
+  gender: {
+    type: String,
+    enum: ['MALE', 'FEMALE'],
+    required: true,
+  }
+});
+
+module.exports = Student = mongoose.model("student", StudentSchema);
