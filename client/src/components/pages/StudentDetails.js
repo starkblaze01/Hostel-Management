52cbc5ceb27fe756802838244c00564c84ec1c2a
Mayank Pathela <mp.pathela@gmail.com>
2019-09-28 20:25:03 +0530

    add more fields in form

--- client/src/components/pages/StudentDetails.js
+++ client/src/components/pages/StudentDetails.js
@@ -18,18 +18,36 @@ class StudentDetails extends Component {
         return (
             <div style={{ height: '100vh' }}>
                 <h1>Batch-2016</h1>
+                <br />
                 <form>
-                    <div className="form-group">
-                        <label for="exampleInputPassword1">Name</label>
-                        <input type="text" className="form-control" id="exampleInputPassword1" placeholder="text" />
+                    <div className="row">
+                        <div className="col">
+                            <label for="exampleInputPassword1">Name</label>
+                            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Name" />
+                        </div>
+                        <div className="col">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        </div>
+                        <div className="col">
+                            <label for="exampleInputPassword1">ID</label>
+                            <input type="number" className="form-control" id="exampleInputPassword1" placeholder="ID" />
+                        </div>
+                        <div className="col">
+                            <label for="exampleInputPassword1">Room No.</label>
+                            <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Room No." />
+                        </div>
+                        <div class="col">
+                            <label for="exampleFormControlSelect1">Gender</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Female</option>
+                                <option>Male</option>
+                            </select>
+                        </div>
+                        <div className="col-auto" >
+                            <button type="submit" style={{ verticalAlign: '-39px' }} className="btn btn-primary">Add</button>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-
-                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         );
