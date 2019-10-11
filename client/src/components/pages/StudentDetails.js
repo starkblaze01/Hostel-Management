093513ac31ab18f8d97ef69f3be2a16834c7cef8
Mayank Pathela <mp.pathela@gmail.com>
2019-10-11 10:41:12 +0530

    add validation

--- client/src/components/pages/StudentDetails.js
+++ client/src/components/pages/StudentDetails.js
@@ -1,45 +1,109 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import classnames from "classnames";
+
 
 class StudentDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            batch: 'Something',
+            batch: 'Batch-2016',
+            name: '',
+            email: '',
+            id: '',
+            gender: '',
+            room: '',
             errors: {}
         }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
-
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const studentDetailsData = {
+            name: this.state.name,
+            email: this.state.email,
+            batch: this.state.batch,
+            id: this.state.id,
+            room: this.state.room,
+            gender: this.state.gender,
+        }
+    }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
     async componentDidMount() {
         // await;
     }
     render() {
+        const { errors } = this.state;
         return (
             <div style={{ height: '100vh' }}>
                 <h1>Batch-2016</h1>
                 <br />
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className="row">
                         <div className="col">
-                            <label for="exampleInputPassword1">Name</label>
-                            <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Name" />
+                            <label for="name">Name</label>
+                            <input type="text" id="name" placeholder="Name"
+                                className={classnames("form-control", {
+                                    "is-invalid": errors.name
+                                })}
+                                onChange={this.onChange}
+                                value={this.state.name}
+                            />
+                            {errors.name && (
+                                <div className="invalid-feedback">{errors.name}</div>
+                            )}
                         </div>
                         <div className="col">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <label for="email">Email address</label>
+                            <input type="email" id="email" aria-describedby="emailHelp" placeholder="Enter email"
+                                className={classnames("form-control", {
+                                    "is-invalid": errors.email
+                                })}
+                                onChange={this.onChange}
+                                value={this.state.email}
+                            />
+                            {errors.email && (
+                                <div className="invalid-feedback">{errors.email}</div>
+                            )}
                         </div>
                         <div className="col">
-                            <label for="exampleInputPassword1">ID</label>
-                            <input type="number" className="form-control" id="exampleInputPassword1" placeholder="ID" />
+                            <label for="id">ID</label>
+                            <input type="number" id="id" placeholder="ID"
+                                className={classnames("form-control", {
+                                    "is-invalid": errors.id
+                                })}
+                                onChange={this.onChange}
+                                value={this.state.id}
+                            />
+                            {errors.id && (
+                                <div className="invalid-feedback">{errors.id}</div>
+                            )}
                         </div>
                         <div className="col">
-                            <label for="exampleInputPassword1">Room No.</label>
-                            <input type="number" className="form-control" id="exampleInputPassword1" placeholder="Room No." />
+                            <label for="room">Room No.</label>
+                            <input type="number" id="room" placeholder="Room No."
+                                className={classnames("form-control", {
+                                    "is-invalid": errors.room
+                                })}
+                                onChange={this.onChange}
+                                value={this.state.room}
+                            />
+                            {errors.room && (
+                                <div className="invalid-feedback">{errors.room}</div>
+                            )}
                         </div>
                         <div class="col">
                             <label for="exampleFormControlSelect1">Gender</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
+                            <select class="form-control" id="exampleFormControlSelect1" onChange={this.onChange} value={this.state.gender}>
                                 <option>Female</option>
                                 <option>Male</option>
                             </select>
