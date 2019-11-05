7e9076a676608c3daf8f8042a8bdb4998d150d86
Mayank Pathela <mp.pathela@gmail.com>
2019-11-05 21:38:46 +0530

    Add path for client build folder

--- server.js
+++ server.js
@@ -38,6 +38,15 @@ app.use("/api/student", students);
 app.use("/api/room", room);
 app.use("/api/staff", staff);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
