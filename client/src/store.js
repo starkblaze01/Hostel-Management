9472fd7abcc29229f4bd66f5809ab7d27a6a8386
Mayank Pathela <mp.pathela@gmail.com>
2019-11-07 18:02:37 +0530

    Disable Redux Extension

--- client/src/store.js
+++ client/src/store.js
@@ -10,8 +10,8 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)//,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
