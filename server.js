46b287b47da7d955ae7f79a3528ef36113ba1bf6
Mayank Pathela <mp.pathela@gmail.com>
2020-06-16 22:08:18 +0530

    supress mongo warning

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const db = require("./config/keys").mongoURI;
 
 // connect to mongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
