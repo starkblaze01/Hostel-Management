fb838d45f549a747c35a4fc3ad047ae045090c02
Mayank Pathela <mp.pathela@gmail.com>
2020-11-02 01:31:39 +0530

    change dep

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-console.log(process.env.NODE_ENV);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
