1f36813c99b3a4d50beb44e87ebf21bad1d0e0ed
Mayank Pathela <mp.pathela@gmail.com>
2020-11-02 00:46:30 +0530

    Add test

--- routers/api/profile.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// @route GET api/users/test
-// @desc Tests profile route
-// @access  Public
-router.get("/test", (req, res) => res.json({ msg: "It works!!" }));
-
-module.exports = router;
