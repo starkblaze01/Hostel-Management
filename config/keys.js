02b45caff386d97aebf22c5e8b652c6fabc50b0b
Mayank Pathela <mp.pathela@gmail.com>
2020-11-02 01:44:23 +0530

    change keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prod");
-} else {
+if (process.env.NODE_ENV === "development") {
   module.exports = require("./keys_dev");
+} else {
+  module.exports = require("./keys_prod");
 }
